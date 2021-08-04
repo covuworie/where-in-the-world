63628c533508a07fc7e27f586389fb6ffe2ea61d
covuworie <chuk.ovuworie@gmail.com>
2021-08-03 23:14:15 -0500

    Move header, country and country-grid

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,19 +5,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CountriesComponent } from './countries/countries.component';
-import { CountryComponent } from './countries/country/country.component';
-import { HeaderComponent } from './header/header.component';
+import { CountryComponent } from './shared/country/country.component';
+import { HeaderComponent } from './shared/header/header.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { CountryDetailComponent } from './countries/country-detail/country-detail.component';
 import {
   Alpha3ToCountryNamePipe,
   Alpha3ToCountryFlagPipe,
-} from './countries/country/alpha-3-to-country.pipe';
-import { LanguagesToNamePipe } from './countries/country/languages-to-names.pipe';
-import { CurrenciesToNameSymbolPipe } from './countries/country/currency-to-name-symbol.pipe';
+} from './shared/country/alpha-3-to-country.pipe';
+import { LanguagesToNamePipe } from './shared/country/languages-to-names.pipe';
+import { CurrenciesToNameSymbolPipe } from './shared/country/currency-to-name-symbol.pipe';
 import { VisitedComponent } from './visited/visited.component';
 import { WishListComponent } from './wish-list/wish-list.component';
-import { CountryGridComponent } from './countries/country-grid/country-grid.component';
+import { CountryGridComponent } from './shared/country-grid/country-grid.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HyphenateUriPipe } from './shared/hyphenate-uri.pipe';
 import { PageNotFoundComponent } from './shared/page-not-found/page-not-found.component';
